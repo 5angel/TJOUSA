9118e30af521a25aa9fb02af2f2946cea084d66c
5angel <vladsenator@gmail.com>
2015-06-09 02:33:55 +0300

    config etc.

--- TJTest/src/ts/app.ts
+++ TJTest/src/ts/app.ts
@@ -1,1 +1,9 @@
-﻿
+﻿/// <reference path="../../typings/tsd.d.ts" />
+
+import MainCtrl = require("controllers/MainCtrl");
+
+var app: ng.IModule = angular.module("test", []);
+
+app.controller("MainCtrl", MainCtrl);
+
+angular.element(document).ready(angular.bootstrap.bind(angular, document, ["test"]));
